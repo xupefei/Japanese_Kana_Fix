dac674fdb94d00d091abb87e152053986a562cd8
Paddy Xu <xupaddy@gmail.com>
2012-07-14 19:46:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+Japanese Kana Fix
+-----------------
+Fix the font of Japanese kana on zh-CN and zh-TW Windows OS.
+
+for Google Chrome 21+.
---
+++ dummy.js
@@ -0,0 +1,9 @@
+chrome.experimental.fontSettings.setFont({"fontName": "", "genericFamily": "standard", "script": "Hans"});
+chrome.experimental.fontSettings.setFont({"fontName": "", "genericFamily": "sansserif", "script": "Hans"});
+chrome.experimental.fontSettings.setFont({"fontName": "", "genericFamily": "serif", "script": "Hans"});
+chrome.experimental.fontSettings.setFont({"fontName": "", "genericFamily": "fixed", "script": "Hans"});
+
+chrome.experimental.fontSettings.setFont({"fontName": "", "genericFamily": "standard", "script": "Hant"});
+chrome.experimental.fontSettings.setFont({"fontName": "", "genericFamily": "sansserif", "script": "Hant"});
+chrome.experimental.fontSettings.setFont({"fontName": "", "genericFamily": "serif", "script": "Hant"});
+chrome.experimental.fontSettings.setFont({"fontName": "", "genericFamily": "fixed", "script": "Hant"});
